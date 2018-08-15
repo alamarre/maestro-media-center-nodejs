54d5aa24be46a045638290227a448daf8440b1c7
Al Lamarre <alamarre@gmail.com>
2018-08-15 08:29:03 -0400

    start fetching collections

--- src/metadata/TheMovieDb.js
+++ src/metadata/TheMovieDb.js
@@ -15,7 +15,15 @@ class TheMovieDb {
         const results = response.results.filter(m => m.title.toLowerCase() === movieName.toLowerCase());
         if(results.length === 1) {
             const result = results[0];
-            return {"source": "TMDB", id: result.id, poster: `http://image.tmdb.org/t/p/original/${result.poster_path}`,};
+            const detailedInfo = await this.movieDb.movieInfo({ id: result.id, });
+            let collectionInfo = null;
+            if(detailedInfo.belongs_to_collection) {
+                const collectionName = detailedInfo.belongs_to_collection.name;
+                const collectionDetails = await this.movieDb.collectionInfo({id: detailedInfo.belongs_to_collection.id,});
+                collectionInfo = {collectionName, movies: collectionDetails.parts.map(m => m.title),};
+
+            }
+            return {"source": "TMDB", collectionInfo,  id: detailedInfo.id, poster: `http://image.tmdb.org/t/p/original/${result.poster_path}`,};
         }
         return {"source": "not found",};
     }
