0411a09c203be5c2cd3838dead8cd609e7cffcad
Al Lamarre <alamarre@gmail.com>
2022-02-26 08:42:10 -0500

    allow JWTs in place of storing sessions

--- src/impl/local/SimplePasswordAuth.js
+++ src/impl/local/SimplePasswordAuth.js
@@ -5,13 +5,26 @@ const resetPercent = 0.1;
 // if we cross the threshold, 10% of the time to expiration we update
 const resetTime = ttl * (1 - resetPercent);
 
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 const ALLOW_ADMIN_READONLY = process.env.ALLOW_ADMIN_READONLY;
+
+const jwt = require("jsonwebtoken");
 
 class SimplePasswordAuth {
   constructor(db) {
     this.db = db;
   }
   async getUser(userToken) {
+    if(PUBLIC_KEY) {
+      try{
+        var decoded = jwt.verify(userToken, PUBLIC_KEY );
+        return decoded;
+      } catch(e) {
+        // fallback to DB
+      }
+    }
     const user = await this.db.get("user_logins", userToken);
     if(user == null) {
       if(ALLOW_ADMIN_READONLY) {
@@ -38,7 +51,11 @@ class SimplePasswordAuth {
     return user;
   }
   async createAuthToken(username, accountId) {
+    if(PRIVATE_KEY) {
+      return jwt.sign({exp:  Math.floor(Date.now()/1000 + ttl),  username, accountId}, PRIVATE_KEY);
+    }
     const token = uuid();
+
     await this.db.set({ username, accountId, expires: Date.now()/1000 + ttl, }, "user_logins", token);
     return token;
   }
