2d88982f20a0d3bf1116275cec3834c3b74c349d
Al Lamarre <al.lamarre@d2l.com>
2023-02-20 15:37:20 -0500

    fix the router

--- src/lambdas/RouteMessages.ts
+++ src/lambdas/RouteMessages.ts
@@ -1,22 +1,22 @@
 import AWS = require("aws-sdk");
 const sns = new AWS.SNS();
 
-const normalize = require("./utilities/EventNormalizer");
+import normalize from "./utilities/EventNormalizer";
 const TOPIC_PREFIX = process.env.TOPIC_PREFIX;
 
 exports.handler = async (event, context, callback) => {
-    await Promise.all(normalize(event).map(async record => {
-        const {table, eventName,} = record;
-        const topic = `${TOPIC_PREFIX}${eventName.toLowerCase()}-${table}`;
-        console.log("publishing to topic", topic);
-        try {
-            await sns.publish({
-                TopicArn: topic,
-                Message: JSON.stringify(record),
-            }).promise();
-        } catch(e) {
-            console.error(e);
-        }
-    }));
-    callback(null, `Successfully processed ${event.Records.length} records.`);
+  await Promise.all(normalize(event).map(async record => {
+    const { table, eventName, } = record;
+    const topic = `${TOPIC_PREFIX}${eventName.toLowerCase()}-${table}`;
+    console.log("publishing to topic", topic);
+    try {
+      await sns.publish({
+        TopicArn: topic,
+        Message: JSON.stringify(record),
+      }).promise();
+    } catch (e) {
+      console.error(e);
+    }
+  }));
+  callback(null, `Successfully processed ${event.Records.length} records.`);
 };
